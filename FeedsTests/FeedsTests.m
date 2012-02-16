2b4b7ec69a9962c557defb8814423111c01e0f1e
Josh Betz <betzster@gmail.com>
2012-02-16 15:23:45 -0600

    Initial Commit

---
+++ FeedsTests/FeedsTests.m
@@ -0,0 +1,32 @@
+//
+//  FeedsTests.m
+//  FeedsTests
+//
+//  Created by Josh Betz on 2/16/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "FeedsTests.h"
+
+@implementation FeedsTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in FeedsTests");
+}
+
+@end
